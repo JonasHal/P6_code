332be216f558885900e8d678ac3473cc4a9d92b5
JonasHal <60646377+JonasHal@users.noreply.github.com>
2022-02-07 10:04:47 +0100

    more data

--- Jonas/showcaseData.py
+++ Jonas/showcaseData.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from pandas import read_csv
 
 data_1hour = read_csv(Path("../Data/electricitybalancenonv_2_1_2022.csv"), sep=',', decimal='.', encoding='latin1')
-data_5mil = read_csv(Path("../Data/electricityprodex5minrealtime_2_1_2022.csv"), sep=',', decimal='.', encoding='latin1')
+data_5min = read_csv(Path("../Data/electricityprodex5minrealtime_2_1_2022.csv"), sep=',', decimal='.', encoding='latin1')
 data_consump = read_csv(Path("../Data/consumptiondk3619codehour_2_1_2022.csv"), sep=',', decimal='.', encoding='latin1')
 
-print(data_1hour.head().to_string())
-print(data_5mil.head().to_string())
-print(data_consump.head().to_string())
+data = [data_5min, data_1hour, data_consump]
+
+for i in data:
+    print(i.head().to_string())
+    print(i.describe().to_string())
