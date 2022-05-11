2c8450f19e63b6447874d0697f29a71327b362bd
JonasHal <60646377+JonasHal@users.noreply.github.com>
2022-05-11 15:22:50 +0200

    Grid Search for Total model

---
+++ Jonas/gs_total_Model.py
@@ -0,0 +1,265 @@
+import math
+import numpy as np
+import pandas as pd
+
+from P6_code.FinishedCode.importData import ImportEV
+from P6_code.FinishedCode.dataTransformation import createTransformation
+from P6_code.FinishedCode.functions import split_sequences, getModelStructure
+from keras.models import Sequential
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+
+class totalModel:
+    """A Model class used to predict one given feature from the total dataframe.
+    @param data: The data to create the model from. Has to be from:
+    createTransformation(*params).getTotalData()
+    @param feature_name: The feature that should be predicted
+    @return: The model object, call createModel() to fit it.
+    """
+    def __init__(self, data, feature_name, n_steps_in, n_nodes, n_nodes_cnn):
+        # Variables to create the model
+        self.totalData = data
+        self.val_split = 0.2
+        self.feature_name = feature_name
+
+        # Scaler
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.mmy = MinMaxScaler(feature_range=(0, 1))
+
+        # Model Hyperparameters (configs)
+        self.model = Sequential()
+        self.n_steps_in = n_steps_in
+        self.n_steps_out = 5
+        self.n_nodes = n_nodes
+        self.n_nodes_cnn = n_nodes_cnn
+
+        self.batch_size = 25
+        self.epochs = 200
+
+    def createModel(self, type="LSTM", layers=2):
+        """Creates the model with the given type and fits the data.
+        @param type: The type of model that should be created. Can be the following:
+        LSTM, GRU, CNN or LSTM-CNN
+
+        @return: The model object, with a fitted model, which can be used for prediction.
+        """
+        print("Making Model")
+        self.layers = layers
+        # Create Input and Target Features
+        X, y = self.totalData.copy(), self.totalData[self.feature_name].values.reshape(-1, 1)
+
+        #Scale og fit the data
+        self.scaler = self.scaler.fit(X)
+        self.mmy = self.mmy.fit(y)
+        X_trans = self.scaler.transform(X)
+        self.y_trans = self.mmy.transform(y)
+
+        # Info about the input features
+        print("The input features are: " + str(X.columns))
+        self.n_features = len(X.columns)
+
+        # Split the data into training and validation data
+        total_samples = len(X)
+        train_val_cutoff = round(self.val_split * total_samples)
+
+        total_X, total_y = split_sequences(X_trans, self.y_trans, self.n_steps_in, self.n_steps_out)
+
+        X_train, X_val = total_X[:-train_val_cutoff], total_X[-train_val_cutoff:]
+        y_train, y_val = total_y[:-train_val_cutoff], total_y[-train_val_cutoff:]
+
+        # Create the model
+        self.title = type
+        self.model = getModelStructure(type, layers, self.n_steps_in, self.n_steps_out, self.n_features, self.n_nodes, self.n_nodes_cnn)
+
+        # Printing the Structure of the model and compile it
+        #print(self.model.summary())
+        self.model.compile(optimizer='adam', loss='mse', metrics=["mean_absolute_error"])
+
+        # Fit the data and trains the model
+        self.history = self.model.fit(x=X_train, y=y_train, batch_size=self.batch_size, epochs=self.epochs, verbose=0,
+                               validation_data=(X_val, y_val))
+
+        # Make and Invert predictions
+        self.trainPredict = self.mmy.inverse_transform(self.model.predict(X_train))
+        trainY = self.mmy.inverse_transform(y_train.reshape(-1, self.n_steps_out))
+        self.valPredict = self.mmy.inverse_transform(self.model.predict(X_val))
+        valY = self.mmy.inverse_transform(y_val.reshape(-1, self.n_steps_out))
+
+        # Calculate the following: Root Mean Squared Error, Mean absolute error and Mean Absolute Percentage Error
+        self.trainRMSE_Score = math.sqrt(mean_squared_error(trainY[:, -1], self.trainPredict[:, -1]))
+        self.trainMAE_Score = mean_absolute_error(trainY[:, -1], self.trainPredict[:, -1])
+
+        self.valRMSE_Score = math.sqrt(mean_squared_error(valY[:, -1], self.valPredict[:, -1]))
+        self.valMAE_Score = mean_absolute_error(valY[:, -1], self.valPredict[:, -1])
+
+        print('Train Score: %.2f RMSE, ' % self.trainRMSE_Score + '%.2f MAE' % self.trainMAE_Score)
+        print('Validation Score: %.2f RMSE, ' % self.valRMSE_Score + '%.2f MAE' % self.valMAE_Score)
+
+        # Return the model and the scalers
+        return self
+
+    def PredictTestSample(self, dataName, start, end):
+        """Note: Should be run after creating the model
+        Predicts a given timeframe from a given dataset.
+        @param dataName: The dataset, where the prediction should take place
+        @param start: The start date of the prediction
+        @param end: The end date of the prediction
+
+        @return: The model object, with the prediction results.
+        """
+        start = str(pd.to_datetime(start) - pd.Timedelta(1, "D"))
+
+        # Import the data
+        if dataName == "Caltech":
+            df = ImportEV().getCaltech(start_date=start, end_date=end)
+        elif dataName == "JPL":
+            df = ImportEV().getJPL(start_date=start, end_date=end)
+        elif dataName == "Office":
+            df = ImportEV().getOffice(start_date=start, end_date=end)
+        elif dataName == "Both":
+            df = ImportEV().getBoth(start_date=start, end_date=end)
+        else:
+            raise Exception("Error, data parameter should be Caltech, JPL, Both or Office")
+
+        total = createTransformation(df, start, end).getTotalData()
+
+        # Create Input and Target Features
+        X, y = total.copy(), total[self.feature_name].values.reshape(-1, 1)
+
+        # Scale the Data
+        X_trans = self.scaler.transform(X)
+        y_trans = self.mmy.transform(y)
+
+        # Split the data for prediction in the RNN models
+        X_test, self.y_test = split_sequences(X_trans, y_trans, self.n_steps_in, self.n_steps_out)
+
+        # Make and Invert predictions
+        self.test_predict = self.mmy.inverse_transform(self.model.predict(X_test))
+
+        # Calculate the following: Root Mean Squared Error, Mean absolute error and Mean Absolute Percentage Error
+        self.testRMSE_Score = math.sqrt(mean_squared_error(self.y_test[:, -1], self.test_predict[:, -1]))
+        self.testMAE_Score = mean_absolute_error(self.y_test[:, -1], self.test_predict[:, -1])
+
+        return self
+
+    def PlotTestSample(self):
+        """Note: Should be run after making a test prediction
+        Makes a graph with the predictions and real values on the test data.
+        """
+
+        # plot baseline and predictions
+        plt.plot(self.y_test[:, 0])
+        plt.plot(self.test_predict[:, 0])
+        plt.title(self.title)
+        plt.show()
+
+    def PlotLoss(self):
+        """Note: Should be run after creating the model
+        Makes a graph with the loss from each epoch when fitting the model.
+        """
+        loss = self.history.history["loss"]
+        val_loss = self.history.history["val_loss"]
+
+        plt.clf()
+        plt.plot(loss, label="train_loss")
+        plt.plot(val_loss, label="val_loss")
+        plt.title(str(self.title) + ', n_in: ' + str(self.n_steps_in) +
+                  ', n_nodes: ' + str(self.n_nodes) + ', n_nodes_cnn: ' + str(self.n_nodes_cnn) + ', layers: ' +
+            str(self.layers) + " - " + str(self.feature_name))
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.savefig(
+            'tm_' + str(self.title) + '/' + str(self.feature_name) + '_n_steps_in' + str(self.n_steps_in) +
+            '_n_nodes' + str(self.n_nodes) + '_n_nodes_cnn' + str(self.n_nodes_cnn) + '_layers' +
+            str(self.layers))
+
+    def PlotTrainVal(self):
+        """Note: Should be run after creating the model
+        Makes a graph with the predictions and real values on the training and validation data.
+        The yellow line is Training
+        The Green line is Validation
+        """
+        # shift train predictions for plotting
+        trainPredictPlot = np.zeros_like(np.concatenate((self.y_trans, np.array([[np.nan]] * self.n_steps_in)), axis=0))
+        trainPredictPlot[:] = np.nan
+        trainPredictPlot[self.n_steps_in:len(self.trainPredict) + self.n_steps_in] = self.trainPredict[:, 0].reshape(-1, 1)
+
+        # shift test predictions for plotting
+        valPredictPlot = np.zeros_like(np.concatenate((self.y_trans, np.array([[np.nan]] * self.n_steps_in)), axis=0))
+        valPredictPlot[:] = np.nan
+        valPredictPlot[len(self.trainPredict) + (self.n_steps_in + self.n_steps_out) : len(self.y_trans) + 1] = self.valPredict[:, 0].reshape(-1, 1)
+
+        # plot baseline and predictions
+        plt.title(self.title)
+        plt.plot(self.mmy.inverse_transform(self.y_trans))
+        plt.plot(trainPredictPlot)
+        plt.plot(valPredictPlot)
+        plt.show()
+
+if __name__ == "__main__":
+    # The model will always be first input
+    start, end = "2018-08-01", "2018-11-01"
+    df = ImportEV().getCaltech(start_date=start, end_date=end, removeUsers=False)
+    Total_df = createTransformation(df, start, end).remove_outliers().getTotalData()
+
+    grid_df = pd.DataFrame(columns=['model type', "target_feature", 'n_steps_in', 'n_nodes', "n_nodes_cnn", "layers", 'train', 'val'])
+
+    for model_type in ["LSTM"]:
+        for feature_name in ["total_kWhDelivered", "carsCharging", "carsIdle"]:
+            for n_steps_in in [3, 15, 50]:
+                for n_nodes in [5, 50, 100]:
+                    if model_type == "LSTM" or model_type == "GRU":
+                        n_nodes_cnn = 32
+                        for layers in [1, 2]:
+                            model = totalModel(Total_df, feature_name, n_steps_in, n_nodes, n_nodes_cnn).createModel(
+                                type=model_type, layers=layers)
+
+                            grid_df = grid_df.append({'model type': model_type, "target_feature": feature_name,
+                                                      'n_steps_in': n_steps_in, 'n_nodes': n_nodes,
+                                                      "n_nodes_cnn": n_nodes_cnn,
+                                                      "layers": layers, 'train': model.trainRMSE_Score,
+                                                      'val': model.valRMSE_Score
+                                                      }, ignore_index=True)
+                            model.PlotLoss()
+                            print({'model type': model_type, "target_feature": feature_name,
+                                   'n_steps_in': n_steps_in, 'n_nodes': n_nodes, "n_nodes_cnn": n_nodes_cnn,
+                                   "layers": layers, 'train': model.trainRMSE_Score, 'val': model.valRMSE_Score
+                                   })
+
+                    else:
+                        for n_nodes_cnn in [32, 64]:
+                            if model_type == "LSTM-CNN":
+                                layers = 1
+                                model = totalModel(Total_df, feature_name, n_steps_in, n_nodes, n_nodes_cnn).createModel(
+                                    type=model_type, layers=layers)
+
+                                grid_df = grid_df.append({'model type': model_type, "target_feature": feature_name,
+                                                          'n_steps_in': n_steps_in, 'n_nodes': n_nodes,
+                                                          "n_nodes_cnn": n_nodes_cnn,
+                                                          "layers": layers, 'train': model.trainRMSE_Score,
+                                                          'val': model.valRMSE_Score
+                                                          }, ignore_index=True)
+                                model.PlotLoss()
+                                print({'model type': model_type, "target_feature": feature_name,
+                                       'n_steps_in': n_steps_in, 'n_nodes': n_nodes, "n_nodes_cnn": n_nodes_cnn,
+                                       "layers": layers, 'train': model.trainRMSE_Score, 'val': model.valRMSE_Score
+                                       })
+                            else:
+                                for layers in [1, 2]:
+                                    model = totalModel(Total_df, feature_name, n_steps_in, n_nodes, n_nodes_cnn).createModel(type=model_type, layers=layers)
+
+                                    grid_df = grid_df.append({'model type': model_type, "target_feature": feature_name,
+                                                              'n_steps_in': n_steps_in, 'n_nodes': n_nodes, "n_nodes_cnn": n_nodes_cnn,
+                                                              "layers": layers, 'train': model.trainRMSE_Score, 'val': model.valRMSE_Score
+                                                              }, ignore_index=True)
+                                    model.PlotLoss()
+                                    print({'model type': model_type, "target_feature": feature_name,
+                                           'n_steps_in': n_steps_in, 'n_nodes': n_nodes, "n_nodes_cnn": n_nodes_cnn,
+                                           "layers": layers, 'train': model.trainRMSE_Score, 'val': model.valRMSE_Score
+                                           })
+
+    print(grid_df.to_string())
+    grid_df.to_csv('gsTotal_df.csv')
